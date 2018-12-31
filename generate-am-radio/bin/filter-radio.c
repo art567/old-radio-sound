c9625c81f61cf1cbd6d5c9e6b0f67fd5bb1498a9
Artyom Shcherbakov <mail@art567.ru>
2018-12-31 01:09:25 +0000

    Interference noise (whistle) level configurable by 5th argument

--- generate-am-radio/bin/filter-radio.c
+++ generate-am-radio/bin/filter-radio.c
@@ -43,8 +43,11 @@ int main (int argc, char *argv[]) {
         bg_signal_fading_extralong_period = rand() % 200000 + 150000;
 
   // Interference noise
-  float interference_level_mkv = 0.14,
-        interference_freq_factor = rand() % 1000 / 1000.0 * 0.1 + 0.13,
+  float interference_level_mkv = 0.14;
+  if (argc > 5) {
+    interference_level_mkv = ( (float) atoi(argv[5] * 0.01 );
+  }
+  float interference_freq_factor = rand() % 1000 / 1000.0 * 0.1 + 0.13,
         interference_fading_fast_period = rand() % 2000 + 1500,
         interference_fading_medium_period = rand() % 7000 + 4000,
         interference_fading_long_period = rand() % 50000 + 25000,
